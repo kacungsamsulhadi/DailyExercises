cbb9dc276e53875716d4a8a0b92c794ed3bf3a53
aninwan <sasuketeme14@gmail.com>
2021-06-12 08:49:44 +0700

    pengisianSession2_yoga

---
+++ lib/subScreenYoga/session2.dart
@@ -0,0 +1,289 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:meditation_app/constants.dart';
+import 'package:meditation_app/widgets/bottom_nav_bar.dart';
+
+class VinyasaYoga extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      bottomNavigationBar: BottomNavBar(),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: size.height * .30,
+            decoration: BoxDecoration(
+              color: kBlueLightColor,
+              image: DecorationImage(
+                image: AssetImage("assets/images/diet_bg.png"),
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      height: size.height * 0.05,
+                    ),
+                    Text(
+                      "Jenis Yoga yang kedua",
+                      style: Theme.of(context)
+                          .textTheme
+                          // ignore: deprecated_member_use
+                          .display1
+                          .copyWith(fontWeight: FontWeight.bold, fontSize: 25),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Vinyasa Yoga",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                    SizedBox(height: 10),
+                    SizedBox(
+                      width: size.width * .6, // it just take 60% of total width
+                      child: Text(
+                        "adalah melakukan pose-pose dalam yoga dengan pernapasan yang sesuai. Antara satu pose dan lainnya harus mengalir sehingga jangan kaget jika gerakan-gerakannya menjadi cepat. \n",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Text("cara melakukan Yoga yang aman dan efektif, di antaranya sebagai berikut:",style:
+                          TextStyle(fontSize: 15),),
+                    SizedBox(height: 0),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 20),
+                      padding: EdgeInsets.all(10),
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(13),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 17),
+                            blurRadius: 23,
+                            spreadRadius: -13,
+                            color: kShadowColor,
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                         
+                          SizedBox(width: 20),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  "Posisikan plank",
+                                  // ignore: deprecated_member_use
+                                  style: Theme.of(context).textTheme.subtitle,
+                                ),
+                                Text("Mulailah dengan posisi plank di mana telapak tangan dan jari-jari kaki menempel pada matras untuk menopang tubuh. Pastikan juga kedua lengan lurus, dan pinggul sejajar dengan bahu hingga membentuk garis lurus dari kepala hingga kaki.")
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 0),
+                      padding: EdgeInsets.all(10),
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(13),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 17),
+                            blurRadius: 23,
+                            spreadRadius: -13,
+                            color: kShadowColor,
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          
+                          SizedBox(width: 20),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  "Turunkan lutut, dada, dan dagu",
+                                  // ignore: deprecated_member_use
+                                  style: Theme.of(context).textTheme.subtitle,
+                                ),
+                                Text("Hembuskan napas untuk menurunkan lutut, dada, dan dagu ke matras. Jangan sampai bokong Anda turun, dan siku harus menekuk lurus ke belakang di sisi tubuh. Pose ini baik untuk tulang punggung dan memperkuat lengan Anda.")
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 20),
+                      padding: EdgeInsets.all(10),
+                      height: 170,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(13),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 17),
+                            blurRadius: 23,
+                            spreadRadius: -13,
+                            color: kShadowColor,
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          
+                          SizedBox(width: 20),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  "Cobra pose",
+                                  // ignore: deprecated_member_use
+                                  style: Theme.of(context).textTheme.subtitle,
+                                ),
+                                Text("Tarik napas dan ubah menjadi posisi cobra di mana tangan tetap pada posisi sebelumnya. Namun, turunkan pinggul dan bokong ke matras sehingga dada terangkat.")
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 15),
+                      padding: EdgeInsets.all(10),
+                      height: 170,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(13),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 17),
+                            blurRadius: 23,
+                            spreadRadius: -13,
+                            color: kShadowColor,
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          
+                          SizedBox(width: 20),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  "Downward facing dog",
+                                  // ignore: deprecated_member_use
+                                  style: Theme.of(context).textTheme.subtitle,
+                                ),
+                                Text("Hembuskan napas dan angkat tubuh Anda di mana bokong berada dalam posisi yang paling tinggi. Bertumpulah pada telapak tangan dan telapak kaki yang menempel pada matras.")
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SeassionCard extends StatelessWidget {
+  final int seassionNum;
+  final bool isDone;
+  final Function press;
+  const SeassionCard({
+    Key key,
+    this.seassionNum,
+    this.isDone = false,
+    this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraint) {
+      return ClipRRect(
+        borderRadius: BorderRadius.circular(13),
+        child: Container(
+          width: constraint.maxWidth / 2 -
+              10, // constraint.maxWidth provide us the available with for this widget
+          // padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(13),
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, 17),
+                blurRadius: 23,
+                spreadRadius: -13,
+                color: kShadowColor,
+              ),
+            ],
+          ),
+          child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: press,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      height: 42,
+                      width: 43,
+                      decoration: BoxDecoration(
+                        color: isDone ? kBlueColor : Colors.white,
+                        shape: BoxShape.circle,
+                        border: Border.all(color: kBlueColor),
+                      ),
+                      child: Icon(
+                        Icons.play_arrow,
+                        color: isDone ? Colors.white : kBlueColor,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      "Session $seassionNum",
+                      // ignore: deprecated_member_use
+                      style: Theme.of(context).textTheme.subtitle,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+    });
+  }
+}
