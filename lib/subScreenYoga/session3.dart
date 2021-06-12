76a7604ae74c1cae21a39f818387ca1feb4dcb6a
aninwan <sasuketeme14@gmail.com>
2021-06-12 08:51:56 +0700

    pengisianSession3_yoga

---
+++ lib/subScreenYoga/session3.dart
@@ -0,0 +1,289 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:meditation_app/constants.dart';
+import 'package:meditation_app/widgets/bottom_nav_bar.dart';
+
+class IyengarYoga extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      bottomNavigationBar: BottomNavBar(),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: size.height * .30,
+            decoration: BoxDecoration(
+              color: kBlueLightColor,
+              image: DecorationImage(
+                image: AssetImage("assets/images/diet_bg.png"),
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      height: size.height * 0.05,
+                    ),
+                    Text(
+                      "Jenis Yoga yang ketiga",
+                      style: Theme.of(context)
+                          .textTheme
+                          // ignore: deprecated_member_use
+                          .display1
+                          .copyWith(fontWeight: FontWeight.bold, fontSize: 25),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Iyengar Yoga",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                    SizedBox(height: 10),
+                    SizedBox(
+                      width: size.width * .6, // it just take 60% of total width
+                      child: Text(
+                        "iyengar yoga pertama kali diperkenalkan oleh B. K. S. Iyengar. Jenis yoga yang satu ini banyak melibatkan keseimbangan, fleksibilitas, dan kekuatan tubuh dengan melakukan pose berpostur tepat. \n",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Text("cara melakukan Yoga yang aman dan efektif, di antaranya sebagai berikut:",style:
+                          TextStyle(fontSize: 15),),
+                    SizedBox(height: 0),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 20),
+                      padding: EdgeInsets.all(10),
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(13),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 17),
+                            blurRadius: 23,
+                            spreadRadius: -13,
+                            color: kShadowColor,
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                         
+                          SizedBox(width: 20),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  "Posenya lebih kompleks. ",
+                                  // ignore: deprecated_member_use
+                                  style: Theme.of(context).textTheme.subtitle,
+                                ),
+                                Text(" Iyengar yoga  membutuhkan kemahiran dan fleksibilitas yang lebih dibanding dengan jenis yoga yang lain. Seringkali, asana harus diajarkan dan dibimbing secara ketat oleh seorang guru yang terlatih.")
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 0),
+                      padding: EdgeInsets.all(10),
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(13),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 17),
+                            blurRadius: 23,
+                            spreadRadius: -13,
+                            color: kShadowColor,
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          
+                          SizedBox(width: 20),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  "Waktu Pose Yang lama",
+                                  // ignore: deprecated_member_use
+                                  style: Theme.of(context).textTheme.subtitle,
+                                ),
+                                Text("Ketika berlatih Iyengar yoga, Anda dituntut untuk melakukan satu pose lebih lama dibanding ketika Anda melakukan pose yang sama pada jenis yoga yang lain.")
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 20),
+                      padding: EdgeInsets.all(10),
+                      height: 170,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(13),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 17),
+                            blurRadius: 23,
+                            spreadRadius: -13,
+                            color: kShadowColor,
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          
+                          SizedBox(width: 20),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  "teknik pernapasan mendalam",
+                                  // ignore: deprecated_member_use
+                                  style: Theme.of(context).textTheme.subtitle,
+                                ),
+                                Text("Pada beberapa jenis yoga lain, pranayama diajarkan segera setelah berlatih asana. Tetapi dalam Iyengar yoga, Mahaguru  BKS Iyengar menekankan bahwa seseorang baru bisa belajar pranayama hanya setelah mereka  menguasai asana.")
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 15),
+                      padding: EdgeInsets.all(10),
+                      height: 170,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(13),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 17),
+                            blurRadius: 23,
+                            spreadRadius: -13,
+                            color: kShadowColor,
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          
+                          SizedBox(width: 20),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  "Menggunakan alat bantu",
+                                  // ignore: deprecated_member_use
+                                  style: Theme.of(context).textTheme.subtitle,
+                                ),
+                                Text("Mahaguru BKS Iyengar menekankan keselarasan dan ketepatan dari semua bagian tubuh saat melakukan asana. Untuk memfasilitasi ini, dia mengembangkan beberapa alat  seperti yoga blok, kursi, selimut dan belt dan tali khusus")
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SeassionCard extends StatelessWidget {
+  final int seassionNum;
+  final bool isDone;
+  final Function press;
+  const SeassionCard({
+    Key key,
+    this.seassionNum,
+    this.isDone = false,
+    this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraint) {
+      return ClipRRect(
+        borderRadius: BorderRadius.circular(13),
+        child: Container(
+          width: constraint.maxWidth / 2 -
+              10, // constraint.maxWidth provide us the available with for this widget
+          // padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(13),
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, 17),
+                blurRadius: 23,
+                spreadRadius: -13,
+                color: kShadowColor,
+              ),
+            ],
+          ),
+          child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: press,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      height: 42,
+                      width: 43,
+                      decoration: BoxDecoration(
+                        color: isDone ? kBlueColor : Colors.white,
+                        shape: BoxShape.circle,
+                        border: Border.all(color: kBlueColor),
+                      ),
+                      child: Icon(
+                        Icons.play_arrow,
+                        color: isDone ? Colors.white : kBlueColor,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      "Session $seassionNum",
+                      // ignore: deprecated_member_use
+                      style: Theme.of(context).textTheme.subtitle,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+    });
+  }
+}
